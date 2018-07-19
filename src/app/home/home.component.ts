b6037d97367c37b34ec5f3dc612759f8a04e3ce2
Tomi Mannila <tomi.mannila@gmail.com>
2018-07-19 16:00:38 +0300

    overall styling changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,24 +9,24 @@ export class HomeComponent implements OnInit {
 
   //title: string = 'Home';
 
-  ItemCount: number;
-  btnText: string = 'Add the item';
-  goalText: string = 'My first item';
+  //ItemCount: number;
+  //btnText: string = 'Add the item';
+  //goalText: string = 'My first item';
 
-  goals = [];
+  //goals = [];
 
   constructor() { }
 
   ngOnInit() {
-    this.ItemCount = this.goals.length;
+    //this.ItemCount = this.goals.length;
   }
 
-  addItem() {
-    this.goals.push(this.goalText);
-    this.goalText = '';
-    this.ItemCount = this.goals.length;
+  // addItem() {
+  //   this.goals.push(this.goalText);
+  //   this.goalText = '';
+  //   this.ItemCount = this.goals.length;
 
 
-  }
+  // }
 
 }
