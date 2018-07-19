2ae0f716de1ca6d9b90f2caf2c58a513064984f8
Tomi Mannila <tomi.mannila@gmail.com>
2018-07-19 16:03:44 +0300

    projectParent is now providing dataStorageService

--- src/app/projects-parent/projects-parent.component.ts
+++ src/app/projects-parent/projects-parent.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ToolService } from '../tool.service';
 import { ProjectService } from '../project.service';
+import { DataStorageService } from '../data-storage.service';
 
 @Component({
   selector: 'app-projects-parent',
   templateUrl: './projects-parent.component.html',
   styleUrls: ['./projects-parent.component.scss'],
-  providers: [ToolService, ProjectService],
+  //providers: [ToolService, ProjectService],
+  providers: [DataStorageService],
 })
 export class ProjectsParentComponent implements OnInit {
 
-  constructor(private toolService: ToolService,
-              private projectService: ProjectService) { }
+  constructor(//private toolService: ToolService,
+              //private projectService: ProjectService,
+              private dataStorageService: DataStorageService) { }
 
   ngOnInit() {
   }
